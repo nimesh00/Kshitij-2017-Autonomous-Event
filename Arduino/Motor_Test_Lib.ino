bef1acfb096e7a659c33a022d0aaa8c58b98bb19
Devnath Nair <devonic99@gmail.com>
2017-01-07 12:57:35 +0300

    Add files via upload
    Use this file if you have the DCMotorBot Library installed. Use the header file or else the library won't work.

---
+++ Arduino/Motor_Test_Lib.ino
@@ -0,0 +1,29 @@
+//Libraries
+#include <DCMotorBot.h> //Library That Drives the Bot. Modify the library if necessary as it uses PWM.
+//Hogaya Kaam
+
+//MOTOR L - 44,45
+//MOTOR R - 46,47
+int inByte;
+
+DCMotorBot bot;
+
+void setup() 
+{     
+    bot.setControlPins(44, 45, 46, 47); //L-->R in Sequence.
+    bot.moveForward();
+  delay(800);
+  bot.turnLeft();
+  delay(800);
+  bot.turnRight();
+  delay(800);
+  bot.moveBackward();
+  delay(800);
+  bot.stop();
+  
+  }
+
+
+void loop() 
+{
+  }
